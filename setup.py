900c9c924ab67fa02b4c0bcb982a43a6d406abaa
taehun.kim <taehun.kim@motional.com>
2023-04-08 08:20:46 -0700

    add dependency

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='w281_final',
-      version='0.01',
-      python_requires='>=3.9',
-      package_dir={'': 'src'},
-      packages=find_namespace_packages(where='src'),
-      install_requires=[
-          
-      ],
+    version='0.01',
+    python_requires='>=3.9',
+    package_dir={'': 'src'},
+    packages=find_namespace_packages(where='src'),
+    install_requires=[
+        'imageio'
+    ],
 )
