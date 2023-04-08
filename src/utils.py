95cb3a1ba57d6ef9cce7386fa8b692caf79f0e41
taehun.kim <taehun.kim@motional.com>
2023-04-07 21:03:17 -0700

    more work on utils

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,46 @@
 import os
+from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Iterable
+from typing import Dict, List
+import imageio.v3 as iio
+import numpy as np
+from itertools import chain
 
-
-class ImageAnnotation:
+class ImageData:
     def __init__(self, filepath: str) -> None:
-        self.label = Path(filepath).parent.name
-        self.filepath = filepath
+        self.filepath = Path(filepath)
+        self.label = self.filepath.parent.name
+        self.augmentation = self.filepath.parent.parent.name
+        # Represents unique id of photo across augmentations
+        self.id = self.filepath.name.split('.')[0].split('-')[0]
+        self.is_sketch = self.filepath.parent.parent.parent.name == 'sketch'
 
-    def load_image(self):
-        pass
+    def load(self) -> np.ndarray:
+        return iio.imread(self.filepath)
 
+@dataclass
+class ImageGroup:
+    id: str
+    label: str
+    photos: List[ImageData] = field(default_factory=list)
+    sketches: List[ImageData] = field(default_factory=list)
 
-def load_images(directory: str) -> Iterable[ImageAnnotation]:
-    assert os.path.isdir(directory), f'{directory} is not a valid directory'
+    def add(self, image: ImageData):
+        group = self.sketches if image.is_sketch else self.photos
+        group.append(image)
 
-    for filepath in Path(directory).rglob('*.png'):
-        yield ImageAnnotation(filepath)
+class ImageDataset(List[ImageGroup]):
+
+    def __init__(self, root_dir: str) -> None:
+        assert os.path.isdir(root_dir), f'{root_dir} is not a valid directory'
+
+        print('Loading dataset...')
+        groups: Dict[str, ImageGroup] = dict()
+        for filepath in chain(Path(root_dir).rglob('*.jpg'), Path(root_dir).rglob('*.png')):
+
+            image = ImageData(filepath)
+            if image.id not in groups:
+                groups[image.id] = ImageGroup(image.id, image.label)
+            groups[image.id].add(image)
+
+        super().__init__(groups.values())
